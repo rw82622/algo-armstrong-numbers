1a3af3a568f7beff893f1db5039d4520c824849f
Rexford Wiafe <w.rexford@yahoo.com>
2022-05-26 16:20:06 -0500

    python solution for armstrong_numbers

--- python/armstrong_numbers.py
+++ python/armstrong_numbers.py
@@ -1,2 +1,13 @@
 # How can you make this more scalable and reusable later?
 def find_armstrong_numbers(numbers):
+    finalArray = []
+    for i in range(len(numbers)):
+        num = list(str(numbers[i]))
+        sum = 0
+        for j in num:
+            sum += pow(int(j), len(num))
+        if sum == int(i):
+            finalArray.append(sum)
+    return finalArray
+
+
