3a195613f6851b46f11884371b91f1e76c26f94b
Rexford Wiafe <w.rexford@yahoo.com>
2022-05-26 16:02:45 -0500

    js solution for armstrongNumbers

--- js/armstrongNumbers.js
+++ js/armstrongNumbers.js
@@ -1,5 +1,21 @@
 // How can you make this more scalable and reusable later?
 
-exports.findArmstrongNumbers = function() {
-
+exports.findArmstrongNumbers = function (anArr) {
+  let resultArr = [];
+  for (let i of anArr) {
+    if (String(i).length === 1) {
+      resultArr.push(i);
+    } else {
+      let splittedNum = String(i).split("");
+      let len = splittedNum.length;
+      let sum = 0;
+      for (let digit of splittedNum) {
+        sum += Number(digit) ** len;
+      }
+      if (sum === i) {
+        resultArr.push(i);
+      }
+    }
+  }
+  return resultArr;
 };
